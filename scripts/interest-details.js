e7a2a39dd4a026f1528ac0f8770df60ec17b7692
Carlos Cereceres <165350098+ce-cereceres@users.noreply.github.com>
2025-06-29 22:30:38 -0600

    Added documentation to update payment and interest forms. Added redirects to the respective loans

--- scripts/interest-details.js
+++ scripts/interest-details.js
@@ -4,25 +4,39 @@ const interestDate = document.querySelector('#interest-date');
 const interestAmount = document.querySelector('#interest-amount');
 // Form
 const interestUpdateForm = document.querySelector('#interest-update-form');
+// "Cancel" button
+const cancelBtn = document.querySelector('#cancel-interest-button');
 
+/**
+ * @typedef {object} InterestDetails
+ * @property {number} id - The unique ID of the interest
+ * @property {number} loan_id - The unique ID of the loan
+ * @property {number} quantity - The amount of the interest
+ * @property {string} renewal - The date when the interest was made
+ */
 
+/**
+ * Recives the interest data.
+ * This function take the interest data to populate the form used to update
+ * @param {InterestDetails} interest
+ */
 window.api.sendInterestDetails((interest) => {
-    console.log(interest);
-    
     interestDate.value = interest.renewal;
     interestAmount.value = interest.quantity;
     
     interestUpdateForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        const interestDetails = {
+        const interestData = {
             id: interest.id,
             loan_id: interest.loan_id,
             quantity: interestAmount.value,
             renewal: interestDate.value
         }
+        window.database.updateInterest(interestData);
+        window.api.getLoanDetails(interest.loan_id);
+    });
 
-        console.log(interestDetails);
-        
-        window.database.updateInterest(interestDetails);
+    cancelBtn.addEventListener('click', () => {
+        window.api.getLoanDetails(interest.loan_id);
     })
 })
--- scripts/payment-details.js
+++ scripts/payment-details.js
@@ -4,24 +4,40 @@ const paymentDate = document.querySelector('#payment-date');
 const paymentAmount = document.querySelector('#payment-amount');
 // Form
 const paymentUpdateForm = document.querySelector('#payment-update-form');
+// "Cancel" button
+const cancelBtn = document.querySelector('#cancel-payment-button');
 
+/**
+ * @typedef {object} PaymentDetails
+ * @property {number} id - The unique ID of the payment
+ * @property {number} loan_id - The unique ID of the loan
+ * @property {number} quantity - The amount of the payment
+ * @property {string} payment_date - The date when the payment was made
+ */
 
-
-// Recieves clicked payment details
+/**
+ * Recives the payment data.
+ * This function take the payment data to populate the form used to update
+ * @param {PaymentDetails} payment
+ */
 window.api.sendPaymentDetails((payment) => {
-    console.log(payment);
     paymentDate.value = payment.payment_date;
     paymentAmount.value = payment.quantity;
     
     paymentUpdateForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        const paymentDetails = {
+        const paymentData = {
             id: payment.id,
             loan_id: payment.loan_id,
             quantity: paymentAmount.value,
             payment_date: paymentDate.value
         }
 
-        window.database.updatePayment(paymentDetails);
+        window.database.updatePayment(paymentData);
+        window.api.getLoanDetails(payment.loan_id);
     })
+
+    cancelBtn.addEventListener('click', () => {
+        window.api.getLoanDetails(payment.loan_id);
+    });
 })
