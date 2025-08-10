d3193e7e4ea2b22032a032c6672fa359ce94cfc6
Carlos Cereceres <165350098+ce-cereceres@users.noreply.github.com>
2025-08-09 21:14:59 -0600

    Deleted old borrower table

--- scripts/borrowers.js
+++ scripts/borrowers.js
@@ -1,7 +1,5 @@
 // "New Borrower" button
 const newBorrowerButton = document.querySelector('#new-borrower-button');
-// Table with borrower
-const borrowerTableBody = document.querySelector('#borrowers-table-body');
 
 // When "New Borrower" button is clicked open borrower form to create new borrower
 newBorrowerButton.addEventListener('click', () => {
@@ -16,7 +14,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         appendAlert(status.message, 'danger')
     } else {
         const borrowersArray = status.data;
-        console.log(borrowersArray);
 
         let borrowerTable = new DataTable('#borrowers-table', {
             data: borrowersArray,
@@ -81,61 +78,6 @@ document.addEventListener('DOMContentLoaded', async () => {
                 }
             }
         });
-        
-        for (const borrower of borrowersArray) {
-            // Table row
-            const tr = document.createElement("tr");
-            // Table data
-            const tdName = document.createElement("td");
-            const tdLastName = document.createElement("td");
-            const tdIne = document.createElement("td");
-            const tdAction = document.createElement("td");
-            // Buttons
-            const deleteButton = document.createElement("button");
-            const updateButton = document.createElement("button");
-            
-            // Appends table row to borrower table
-            borrowerTableBody.appendChild(tr);
-            // Set values to table data
-            tdName.textContent = borrower.name;
-            tdLastName.textContent = borrower.last_name;
-            tdIne.textContent = borrower.ine;
-            // Delete button
-            deleteButton.value = borrower.id;
-            deleteButton.textContent = `Delete id = ${borrower.id}`;
-            deleteButton.classList.add('btn', 'btn-danger')
-            // Update button
-            updateButton.value = borrower.id;
-            updateButton.textContent = `Update id = ${borrower.id}`;
-            updateButton.classList.add('btn', 'btn-primary')
-            // Appends table data to table row
-            tr.appendChild(tdName);
-            tr.appendChild(tdLastName);
-            tr.appendChild(tdIne);
-            tr.appendChild(tdAction);
-            // Appends buttons to table data "Action"
-            tdAction.appendChild(updateButton);
-            tdAction.appendChild(deleteButton);
-
-            // Action on clicked "Delete" button
-            deleteButton.addEventListener('click', () => {
-                const userConfirmed = confirm('Are you sure you want to delete this borrower?');
-                if (userConfirmed) {
-                    const doubleUserConfirmed = confirm('All loans from this borrower will be lost. Are you sure you want to continue?');
-                    if (doubleUserConfirmed) {
-                        // Delete the borrower
-                        window.database.deleteBorrower(deleteButton.value);
-                        // Reloads the page
-                        location.reload();
-                    }
-                }
-            });
-            // Action on clicked "Update" button
-            updateButton.addEventListener('click', () => {
-                // Opens form to update borrower
-                window.api.openBorrowerDetails(updateButton.value)
-            });
-        }
     }
 })
 
